5206dc8ba3aac7a1d704eea930b8085cfce29ca6
hector.ramirez.banda <hector.ramirez.banda@MTYTM1C61XWL1>
2017-04-18 16:31:57 -0500

    Add project files.

---
+++ SonarQubeTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SonarQubeTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
